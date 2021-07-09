4f9b88ed460cccde38e57c8044d2836ec71efc82
CNGAMERPGG <65620661+CNGAMERPGG@users.noreply.github.com>
2021-07-09 22:37:51 +0800

    tabbar

---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,47 @@
+<template>
+    <tab-bar>
+      <tab-bar-item path="/home" activeColor='deepPink'>
+        <img slot="item-icon" src="@/assets/img/tabbar/home.svg" alt="">
+        <img slot="item-icon-active" src="@/assets/img/tabbar/home_active.svg" alt="">
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+      <tab-bar-item path="/category" activeColor='deepPink'>
+        <img slot="item-icon" src="@/assets/img/tabbar/category.svg" alt="">
+        <img slot="item-icon-active" src="@/assets/img/tabbar/category_active.svg" alt="">
+        <div slot="item-text" >精选</div>
+      </tab-bar-item>
+      <tab-bar-item path="/cart" activeColor='deepPink'> 
+        <img slot="item-icon" src="@/assets/img/tabbar/shopcart.svg" alt="">
+        <img slot="item-icon-active" src="@/assets/img/tabbar/shopcart_active.svg" alt="">
+        <div slot="item-text" >购物车</div>
+      </tab-bar-item>
+      <tab-bar-item path="/profile" activeColor='deepPink'>
+        <img slot="item-icon" src="@/assets/img/tabbar/profile.svg" alt="">
+        <img slot="item-icon-active" src="@/assets/img/tabbar/profile_active.svg" alt="">
+        <div slot="item-text" >我的</div>
+      </tab-bar-item>
+    </tab-bar>
+</template>
+
+<script>
+import TabBar from '../../common/tabbar/TabBar.vue'
+import TabBarItem from '../../common/tabbar/TabBarItem'
+// import TabBar from '@/components/common/tabbar/TabBar.vue'
+// import TabBar from 'components/common/tabbar/TabBar.vue'
+
+  // import TabBar from 'E:/vscode/test-vuecli/supermall/src/components/common/tabbar/TabBar'
+  // import TabBarItem from 'E:/vscode/test-vuecli/supermall/src/components/common/tabbar/TabBarItem'
+
+export default {
+    name: "MainTabBar",
+    components:{
+      TabBar,
+      TabBarItem
+    }
+}
+        TabBar
+</script>
+
+<style scoped>
+
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,37 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
+const Home = () => import('../views/home/Home')
+const Cart = () => import('../views/cart/Cart')
+const Category = () => import('../views/category/Category')
+const Profile = () => import('../views/profile/Profile')
+
+// 1.安装插件
 Vue.use(Router)
 
+// 2.创建路由对象
 export default new Router({
-  
+  routes: [
+    {
+      path: '',
+      redirect: '/home'
+    },
+    {
+      path: '/home',
+      component: Home
+    },
+    {
+      path: '/cart',
+      component: Cart
+    },
+    {
+      path: '/category',
+      component: Category
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+  ],
+  mode: 'history'
 })
