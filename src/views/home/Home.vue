b640a3993f4ebf5127f573ae959ae6e8e5244a39
CNGAMERPGG <917166997@qq.com>
2021-07-17 00:08:21 +0800

    scroll

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -4,7 +4,10 @@
         <home-swiper :banners="banners"/>
         <recommend-view :recommends="recommends"></recommend-view>
         <feature-view></feature-view>
-        <tab-control :titles="['流行', '新款', '精选']" class="tab-control"></tab-control>
+        <tab-control :titles="['流行', '新款', '精选']" 
+                     class="tab-control"
+                     @tabClick="tabClick" />
+        <good-list :goods="showGoods"/>
     </div>
 </template>
 
@@ -19,6 +22,7 @@
     import RecommendView from './childComps/RecommendView'
     import FeatureView from './childComps/FeatureView.vue'
     import TabControl from '../../components/content/tabControl/TabControl.vue'
+    import GoodList from '../../components/content/goods/GoodList.vue'
 
     export default {
         name: "Home",
@@ -28,9 +32,10 @@
                 recommends: [],
                 goods: {
                     'pop': {page: 0, list: []},
-                    'news': {page: 0, list: []},
+                    'new': {page: 0, list: []},
                     'sell': {page: 0, list: []},
-                }
+                },
+                currentType: 'pop'
             }
         },
         components: {
@@ -39,6 +44,12 @@
             RecommendView,
             FeatureView,
             TabControl,
+            GoodList,
+        },
+        computed: {
+            showGoods() {
+                return this.goods[this.currentType].list
+            }
         },
         created(){
             // 1. 请求多个数据
@@ -50,9 +61,30 @@
             this.getHomeGoods('sell')
         },
         methods: {
+            /**
+             * 事件监听相关方法
+             */
+            tabClick(index) {
+                switch (index) {
+                    case 0:
+                        this.currentType = 'pop'
+                        break
+                    case 1:
+                        this.currentType = 'new'
+                        break
+                    case 2:
+                        this.currentType = 'sell'
+                        break
+                    
+                }
+            },
+
+
+            /**
+             * 网络请求相关方法
+             */
             getHomeMultidata() {
                 getHomeMultidata().then(res => {
-                    console.log(res);
                     this.banners = res.data.banner.list;
                     this.recommends = res.data.recommend.list;
                 })
@@ -64,7 +96,7 @@
                     this.goods[type].list.push(...res.data.list)
                     this.goods[type].page += 1
                 })
-           }
+           },
         }
     }
 </script>
